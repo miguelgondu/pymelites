2f4aa25db6006fe2e028a6d22acd6a9991d1efa8
Miguel Gonzalez Duque <migd@itu.dk>
2020-03-02 11:49:20 +0100

    Implements visualization using partition to select the features

--- pymelites/visualizing_generations.py
+++ pymelites/visualizing_generations.py
@@ -24,29 +24,42 @@ def get_plot_params(filepaths):
 
     return vmin, vmax
 
-def _plot_generation(filepath, partitions=None, xlims=None, ylims=None, vmin=None, vmax=None):
+def _plot_generation(filepath, partition=None, vmin=None, vmax=None):
     with open(filepath) as fp:
         generation = json.load(fp)
 
-    if xlims is None and ylims is None:
-        partitions_items = list(partitions.items())
-        partitions_items.sort(key=itemgetter(0))
-        xlims = partitions_items[0][1][:2]
-        ylims = partitions_items[1][1][:2]
-    
+    # Getting the plotting parameters from the partition.
+    partition_items = list(partition.items())
+    partition_items.sort(key=itemgetter(0))
+
+    key_x = partition_items[0][0]
+    key_y = partition_items[1][0]
+    xlims = partition_items[0][1][:2]
+    ylims = partition_items[1][1][:2]
+
     points = np.zeros((len(generation), 2))
     colors = np.zeros(len(generation))
-    # How to deal with None's?
+
     i = 0
     for doc in generation.values():
         if doc["performance"] is None:
             np.delete(points, i, axis=0)
             np.delete(colors, i)
-            # points[i, :] = doc["centroid"]
-            # colors[i] = 0
-            # i += 1
         else:
-            points[i, :] = doc["centroid"]
+            if isinstance(doc["features"], (list, tuple, np.ndarray)):
+                index_x, index_y = key_x, key_y
+            elif isinstance(doc["features"], dict):
+                # Sort the keys in doc["features"]
+                keys = list(doc["features"].keys())
+                keys.sort()
+
+                # Pick the index_x and index_y according to the positions
+                # of key_x and key_y in the sorted array, because the
+                # centroid was built in alphabetical order.
+                index_x, index_y = keys.index(key_x), keys.index(key_y)
+            else:
+                raise RuntimeError(f"Features is of unexpected type {type(doc['features'])}")
+            points[i, :] = (doc["centroid"][index_x], doc["centroid"][index_y])
             colors[i] = doc["performance"]
             i += 1
     
@@ -54,9 +67,9 @@ def _plot_generation(filepath, partitions=None, xlims=None, ylims=None, vmin=Non
     scatter = ax.scatter(points[:, 0], points[:, 1], c=colors, vmin=vmin, vmax=vmax)
     ax.set_xlim(xlims)
     ax.set_ylim(ylims)
-    if partitions is not None:
-        ax.set_xlabel(partitions_items[0][0])
-        ax.set_ylabel(partitions_items[1][0])
+    if isinstance(partition_items[0][0], str):
+        ax.set_xlabel(partition_items[0][0])
+        ax.set_ylabel(partition_items[1][0])
     plt.colorbar(scatter)
 
     title = get_name_from_path(filepath).replace("_", " ")
@@ -65,7 +78,7 @@ def _plot_generation(filepath, partitions=None, xlims=None, ylims=None, vmin=Non
     # plt.show()
     plt.close()
 
-def plot_generations(filepaths, partitions=None, xlims=None, ylims=None):
+def plot_generations(filepaths, partition=None):
     """
     This function takes an iterable with the paths of
     the generation_{d}.json outputted by the MAP_Elites
@@ -74,17 +87,15 @@ def plot_generations(filepaths, partitions=None, xlims=None, ylims=None):
 
     Input:
         - filepaths
-        - partitions.
+        - partition.
     Output:
         None, but it creates images.
     """
     files = list(glob.glob(filepaths))
     vmin, vmax = get_plot_params(files)
 
-
     for i, filepath in enumerate(files):
         print(f"{i+1}/{len(files)}")
         _plot_generation(
-            filepath, partitions=partitions, xlims=xlims, ylims=ylims,
-            vmin=vmin, vmax=vmax
+            filepath, partition=partition, vmin=vmin, vmax=vmax
         )
