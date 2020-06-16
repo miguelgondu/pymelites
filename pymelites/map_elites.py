3a7481e037fe0a65f78399789c79917325c1a6d4
Miguel Gonzalez Duque <migd@itu.dk>
2020-06-16 22:58:15 +0200

    Re-writes the documentation in the core MAP-Elites document

--- pymelites/map_elites.py
+++ pymelites/map_elites.py
@@ -2,8 +2,10 @@
 This script creates a class for a generic MAP_Elites implementation.
 
 See:
-    - Illuminating search spaces by mapping elites. https://arxiv.org/abs/1504.04909
-    - The original pymap-elites implementation by J.B. Mouret et al. 
+    - Illuminating search spaces by mapping elites.
+    https://arxiv.org/abs/1504.04909
+    - The original pymap-elites implementation by J.B. Mouret et al.
+    https://gitlab.inria.fr/resibots/public/py_map_elites
 '''
 import itertools
 import json
@@ -15,12 +17,13 @@ from operator import itemgetter
 
 class Cell:
     '''
-    This cell object maintains the current elite in the cell, its genotypical description
-    in self.solution, its feature description in self.features and its features in
-    self.features. It also maintains a certain amount of elites in each cell.
+    This cell object maintains the current elite in the cell,
+    its genotypical description in self.solution, its feature
+    description in self.features and its features in self.features.
+    It also maintains a certain amount of elites in each cell.
 
-    Cells are usually indexed by their centroid. The centroid acts as an identifier
-    in the MAP_Elites self.cells object.
+    Cells are usually indexed by their centroid. The centroid acts as
+    an identifier in the MAP_Elites self.cells object.
     '''
     def __init__(self, centroid, amount_of_elites=3, metadata={}):
         self.centroid = centroid
@@ -32,6 +35,11 @@ class Cell:
         self.metadata = metadata
 
     def add_to_elites(self, genotype, performance):
+        """
+        This function adds a genotype to a set of elites by
+        checking if it has a better performance than the ones
+        currently in the archive.
+        """
         genotype = tuple_to_string(tuple(genotype))
         if len(self.elites) < self.amount_of_elites:
             self.elites[genotype] = performance
@@ -42,9 +50,23 @@ class Cell:
             self.elites = dict(sorted_items[:self.amount_of_elites])
 
     def to_dict(self):
-        # TODO: Once the generic functions in utils have been implemented, add them here.
+        """
+        This function returns a serialized version of
+        this cell. This dict has the following keys:
+
+        {
+            "centroid": cell's centroid (tuple),
+            "solution": best performing genotype (you define its type),
+            "features": solution's features (tuple or dict),
+            "performance": solution's performance (number-like),
+            "elites": an archive of the best elites (dict),
+            "metadata": metadata passed by simulate (yours to define)
+        }
+        """
+        # TODO: Once the generic functions for serializing
+        # have been implemented, add them here.
         if self.solution is not None:
-            if isinstance(self.solution, list) or isinstance(self.solution, np.ndarray):
+            if isinstance(self.solution, (list, np.ndarray)):
                 solution = tuple(self.solution)
             elif isinstance(self.solution, dict):
                 solution = self.solution
@@ -52,16 +74,15 @@ class Cell:
             solution = None
 
         if self.features is not None:
-            if isinstance(self.features, list) or isinstance(self.features, np.ndarray):
+            if isinstance(self.features, (list, np.ndarray)):
                 features = tuple(self.features)
             elif isinstance(self.features, dict):
                 features = self.features
         else:
             features = None
-        # print(f"self.features: {self.features}")
-        # print(f"features: {features}")
+
         document = {
-            "centroid": tuple(self.centroid), # This one's not really necessary
+            "centroid": tuple(self.centroid),
             "solution": solution,
             "features": features,
             "performance": self.performance,
@@ -84,15 +105,20 @@ class Cell:
 class MAP_Elites:
     def __init__(self, random_solution, random_selection, random_variation, simulate):
         '''
-        Documentation: the initialization of a MAP_Elites object.
+        The initialization of a MAP_Elites object. It takes as input
         - random_solution() returns a random "genotype".
-        - random_selection(X) grabs an element at random from a set of solutions.
+        - random_selection(X) grabs an element at random from a set
+          of solutions.
         - random_variation(x) mutates the genotype x.
-        - simulate(x) grabs a genotype x and simulates it, recording a low-dimensional feature
-          description and a performance. It should return a tuple (performance(x), feature_description(x)).
+        - simulate(x) grabs a genotype x and simulates it, recording a
+          low-dimensional feature description and a performance. It
+          should return either a tuple (performance(x), features(x)) or
+          a tuple (performance(x), features(x), metadata(x)). This
+          metadata will be stored in the cell for the best performing
+          solution (genotype).
 
-        TODO: 
-            - expose the amount of elites to maintain. The kwargs are so many though. (!)
+          The features(x) should return a dict {feature: value} with the
+          same features as the partition specified while creating cells.
         '''
         self.random_solution = random_solution
         self.random_selection = random_selection
@@ -111,27 +137,24 @@ class MAP_Elites:
         the archive once `compute_archive` is used.
 
         It takes:
-            - partition: a description of how to divide the feature
-              space, it's a dictionary {
-                    "feat_1": (lower_bound, upper_bound, amount_of_cells),
-                    ...
-                    "feat_n": (lower_bound, upper_bound, amount_of_cells)
-                }, with this partition the cells will be created in a gridlike
-                fashion, and each one of them identified by their centroid.
-            - amount_of_elites: an integer stating how many elites are going to
-              be kept for each cell. Each cell can keep multiple elites (which
-              can be useful when you want to store a certain amount of high-performing
-              genotypes in each cell).
+        - partition: a description of how to divide the feature
+          space, it's a dictionary {
+                "feat_1": (lower_bound, upper_bound, amount_of_cells),
+                ...
+                "feat_n": (lower_bound, upper_bound, amount_of_cells)
+          }, with this partition the cells will be created in a gridlike
+          fashion, and each one of them identified by their centroid. The
+          features will be ordered alphabetically when performing computations.
+        - amount_of_elites: an integer stating how many elites are going to
+          be kept for each cell. Each cell can keep multiple elites (which
+          can be useful when you want to store a certain amount of
+          high-performing genotypes in each cell).
 
         Once it is run, the following attributes are stored in the class:
-            - self.cells: a dict {centroid: Cell object}.
-            - self.centroids: an array with the centroids of all cells.
-            - self.centroids_tree: a KDTree built with the centroids for fast
-              closest neighbor querying.
-
-        TODO: fix this docstring. It's no longer taking a dict, but rather
-        a list of all tuples. With the new feature implementation, it would
-        go back to taking a dict.
+        - self.cells: a dict {centroid: Cell object}.
+        - self.centroids: an array with the centroids of all cells.
+        - self.centroids_tree: a KDTree built with the centroids for fast
+            closest neighbor querying.
         '''
         midpoints = {}
         for feature, tuple_ in partition.items():
@@ -146,19 +169,6 @@ class MAP_Elites:
         self.cells = cells
         self.centroids = np.array(list(self.cells.keys()))
         self.centroids_tree = KDTree(self.centroids)
-
-        # Creating the midpoints
-        # midpoints = {}
-        # for feature, tuple_ in enumerate(partition):
-        #     a, b, n = tuple_
-        #     h = (b - a)/(n - 1)
-        #     midpoints[feature] = np.linspace(a + (1/2)*h, b - (1/2)*h, n - 1)
-
-        # centroids = itertools.product(*midpoints.values())
-        # cells = {centroid: Cell(centroid, amount_of_elites) for centroid in centroids}
-        # self.cells = cells
-        # self.centroids = np.array(list(self.cells.keys()))
-        # self.centroids_tree = KDTree(self.centroids)
     
     def create_cells_CVT(self, partition, amount_of_elites, samples=25000):
         '''
@@ -173,10 +183,10 @@ class MAP_Elites:
                     (lower_bound_fn, upper_bound_fn, amount_of_cells_fn)
                 ], with this partition the cells will be created, each 
                 one of them identified by their centroid.
-          - amount_of_elites: an integer stating how many elites are going to
-            be kept for each cell. Each cell can keep multiple elites (which
-            can be useful when you want to store a certain amount of high-performing
-            genotypes in each cell).
+          - amount_of_elites: an integer stating how many elites are going
+            to be kept for each cell. Each cell can keep multiple elites
+            (which can be useful when you want to store a certain amount
+            of high-performing genotypes in each cell).
         '''
 
         # Sampling random points and running kmeans
@@ -184,50 +194,58 @@ class MAP_Elites:
         # linear search being performed below.
         amount_of_cells = max([tuple_[2] for tuple_ in partition])
         feature_space_dim = len(partition)
-        print(f"Sampling {samples} for the CVT creation.")
+        # print(f"Sampling {samples} for the CVT creation.")
         random_samples = np.zeros((samples, feature_space_dim))
         for i, tuple_ in enumerate(partition):
             random_samples[:, i] = np.random.uniform(tuple_[0], tuple_[1], samples)
-        
-        print(f"Finding the centroids using KMeans.")
+
+        # print(f"Finding the centroids using KMeans.")
         self.centroids = KMeans(n_clusters=amount_of_cells).fit(random_samples).cluster_centers_
-        print(f"Computing the KDTree.")
+        # print(f"Computing the KDTree.")
         self.centroids_tree = KDTree(self.centroids)
 
         # Creating the cell objects
         # TODO: the tuple stuff is dumb, I should find a better way.
-        cells = {tuple(centroid): Cell(tuple(centroid), amount_of_elites) for centroid in self.centroids}
+        cells = {
+            tuple(centroid): Cell(tuple(centroid), amount_of_elites) for centroid in self.centroids
+        }
         self.cells = cells
-        print(f"Cells successfully created.")
+        # print(f"Cells successfully created.")
 
     def _get_tuple_from_feature_dict(self, b):
-        # Transforms dict into tuple by considering the keys
-        # in alphabetical order.
+        """
+        Transforms dict into tuple by considering the keys
+        in alphabetical order.
+        """
         keys = list(b.keys())
         keys.sort()
         return tuple([b[key] for key in keys])
 
     def get_cell(self, b):
         """
-        Gets the cell corresponding to features b. Takes a dict
-        or a tuple-like.
+        Gets the cell corresponding to features b.
+        Takes a dict or a array-like.
         """
         if isinstance(b, dict):
             # Transform it to a tuple-like.
             b_tuple = self._get_tuple_from_feature_dict(b)
         else:
             b_tuple = b
-        # print(b_tuple)
+        
+        # Queries the KDTree for the closest centroid in
+        # the grid defined by the partition, and returns
+        # the respective cell.
         centroid_pos = self.centroids_tree.query(b_tuple)[1]
         centroid = self.centroids_tree.data[centroid_pos]
         return self.cells[tuple(centroid)]
 
     def process_solution(self, x_prime, metadata={}):
         '''
-        In the main body of the MAP-Elites algorithm, a "simulation" is performed to get
-        both the feature description of the genotype x_prime and the performance p_prime. Once
-        these are computed, the archive is maintained by adding the solution to the respective
-        cell if they are the new elite.
+        In the main body of the MAP-Elites algorithm, a "simulation"
+        is performed to get both the feature description of the
+        genotype x_prime and the performance p_prime. Once these are
+        computed, the archive is maintained by adding the solution
+        to the respective cell if they are the new elite.
         '''
         _tuple = self.simulate(x_prime)
         if len(_tuple) == 2:
@@ -236,10 +254,9 @@ class MAP_Elites:
             # Player has passed metadata for the cell
             p_prime, b_prime, metadata = _tuple
         else:
-            raise RuntimeError("The simulate function should return 2 (p, features) or 3 objects (p, features, metadata).")
+            raise RuntimeError("The simulate function should return (p, features) or (p, features, metadata).")
 
         cell = self.get_cell(b_prime)
-        # print(f"cell: {cell}")
         # Update the cell's attributes &
         # Maintain the list of current solutions
         if cell.solution is None or cell.performance < p_prime:
@@ -253,16 +270,34 @@ class MAP_Elites:
 
     def compute_archive(self, generations, iterations_per_gen, initial_iterations=None, generation_path='.', save_each_gen=True, comment=""):
         '''
-        This function computes the archive, which is stored in the self.cells object.
+        This function computes the archive, which is stored
+        in the self.cells object.
 
-        Each cell maintains the best performenace, the best solution x (a.k.a genotype) and
-        the feature description of this best genotype.
+        Each cell maintains the best performenace, the best
+        solution x (also called genotype), the feature
+        description of this best genotype and also metadata.
 
-        This computation follows (loosely) the notation in https://arxiv.org/abs/1504.04909.
+        This computation follows (loosely) the notation
+        in https://arxiv.org/abs/1504.04909.
+
+        Input:
+        - generations: number of generations (int).
+        - iterations_per_gen: number of iterations
+          per generation (int).
+        - initial_iterations: number of iterations
+          in the initial generation (the one that
+          creates the first random genotypes).
+        - generation_path: path in which
+          the generation files will be saved. (str)
+        - save_each_gen: whether or not to save
+          each generation. (bool)
+        - comment: string that will be included
+          in the name of the generations. (str)
 
         TODO:
             - change the amount of zeros in the saving of the file dynamically given 'generations'
             - Implement parallelization
+            - make the generation path OS-independent.
         '''
         if self.cells is None:
             # TODO: this should be better worded, or better implemented.
@@ -270,13 +305,13 @@ class MAP_Elites:
         for g in range(generations):
             print(f"="*80)
             print(f"Generation: {g}")
-            # Initialization
+
+            # Initialization loops
             if initial_iterations is None:
                 initial_iterations = iterations_per_gen
             if g == 0:
                 for it in range(initial_iterations):
                     # We get a random genotype.
-                    # TODO: this is a hack, remove it when the PCG stuff has been addressed
                     print(f"-"*80)
                     print(f"Iteration: {it}")
                     while True:
@@ -287,48 +322,56 @@ class MAP_Elites:
                             print("WARNING: There is something wrong with the random solution creator.")
                             pass
 
-                    # We compute the descriptors and add it to the relevant cell.
-                    # TODO: this is a hack, remove it when the PCG stuff has been addressed.
+                    # We compute the descriptors and add them
+                    # to the relevant cell.
                     try:
                         self.process_solution(x_prime)
                     except ValueError:
                         print(f"Couldn't process genotype {x_prime}. Check your random creators and mutators.")
                         continue
+
             # Update loops
-            else:
+            else: # g > 0
                 for it in range(iterations_per_gen):
                     # Variations to the x_prime.
-                    # TODO: the random selection can be sped up by working with a set, right?
                     print(f"-"*80)
                     print(f"Iteration: {it}")
                     x = self.random_selection(list(self.solutions.values()))
-                    # TODO: this is a hack, remove it when the PCG stuff has been addressed.
+
+                    # Attempts to mutate a genotype 5 different times.
                     for _ in range(5):
                         try:
                             x_prime = self.random_variation(x)
                             break
                         except ValueError:
-                            print(f"Couldn't mutate genotype {x}. Check your random creators and mutators.")
+                            print(f"WARNING: Couldn't mutate genotype {x}. Check your random creators and mutators.")
                     else:
+                        # i.e. couldn't mutate x_prime, so the code
+                        # continues the computation of the archive.
                         continue
 
-                    # and again we compute the descriptors and add the solution to the relevant cell
-                    # TODO: this is a hack, remove it when the PCG stuff has been addressed.
+                    # and again we compute the descriptors and add
+                    # the solution to the relevant cell
                     try:
                         self.process_solution(x_prime)
                     except ValueError:
-                        print(f"Couldn't process genotype {x_prime}. Check your random creators and mutators.")
+                        print(f"WARNING: Couldn't process genotype {x_prime}. Check your random creators and mutators.")
                         continue
 
             if save_each_gen:
-                self.write_cells(generation_path + f"/generation_{comment}_{g:05d}.json")
+                self.write_cells(
+                    generation_path + f"/generation_{comment}_{g:09d}.json"
+                )
 
             if g == generations - 1 and not save_each_gen:
-                self.write_cells(generation_path + f"/generation_{comment}_{g:05d}.json")
+                self.write_cells(
+                    generation_path + f"/generation_{comment}_{g:09d}.json"
+                )
 
     def write_cells(self, path):
         '''
-        This function writes what's on the self.cells object into readable JSON.
+        This function writes what's on the self.cells
+        object into readable JSON.
         '''
         document = {
             tuple_to_string(centroid): cell.to_dict() for centroid, cell in self.cells.items()
@@ -340,5 +383,6 @@ class MAP_Elites:
 def tuple_to_string(tuple_):
     '''
     This function takes a tuple and converts it to a string.
+    This string is JSON parsable.
     '''
     return str(list(tuple_))
